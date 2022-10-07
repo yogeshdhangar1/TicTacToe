2475d67139b5809cf8eef74139cf9c8253b88949
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-10-07 00:41:58 -0700

    Added: Win Tie

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -22,25 +22,60 @@ public class TicTacToe {
         System.out.println(" "+ board[4]+"__|"+board[5]+"___|__"+board[6]);
         System.out.println(" "+ board[7]+"  |"+board[8]+"   |"+board[9]);
     }
-    static void makeMove(){
-        int playLocation;
-        System.out.println("Enter The Location Make Move 1 To 9");
-        playLocation = sc.nextInt();
-        if(playLocation < 10 && playLocation>0){
-            board[playLocation] = playerLetter;
-        }else{
-            System.out.println("Invalid Choice");
+    static void makeMove(int position, char currentPlayerLetter, char[] board) {
+
+        switch (position) {
+            case 1:
+                if (board[1] == ' ')
+                    board[1] = currentPlayerLetter;
+                break;
+            case 2:
+                if (board[2] == ' ')
+                    board[2] = currentPlayerLetter;
+                break;
+            case 3:
+                if (board[3] == ' ')
+                    board[3] = currentPlayerLetter;
+                break;
+            case 4:
+                if (board[4] == ' ')
+                    board[4] = currentPlayerLetter;
+                break;
+            case 5:
+                if (board[5] == ' ')
+                    board[5] = currentPlayerLetter;
+                break;
+            case 6:
+                if (board[6] == ' ')
+                    board[6] = currentPlayerLetter;
+                break;
+            case 7:
+                if (board[7] == ' ')
+                    board[7] = currentPlayerLetter;
+                break;
+            case 8:
+                if (board[8] == ' ')
+                    board[8] = currentPlayerLetter;
+                break;
+            case 9:
+                if (board[9] == ' ')
+                    board[9] = currentPlayerLetter;
+                break;
+            default:
+                break;
         }
     }
+
     static void playerMove() {
-        System.out.println("Enter choice 1 to 9");
-        int position = sc.nextInt();
+        System.out.println("Enter the position(1-9)");
+        int  position = sc.nextInt();
         while (board[position] != ' ') {
             System.out.println("Invalid option!! Please Try Again");
             position = sc.nextInt();
         }
-        makeMove();
-        System.out.println("Player Moved  :"+position);
+        makeMove(position, playerLetter, board);
+        System.out.println("Player moved to"+position);
+        isGameOver(board,playerLetter);
     }
     static Player toss() {
         if((int)(Math.random()*10)%2==1){
@@ -52,12 +87,36 @@ public class TicTacToe {
             return Player.COMPUTER;
         }
     }
+    public static boolean isGameOver(char[] board, char letter) {
+
+        boolean option1 = (board[1] == letter && board[2] == letter && board[3] == letter);
+        boolean option2 = (board[4] == letter && board[5] == letter && board[6] == letter);
+        boolean option3 = (board[7] == letter && board[8] == letter && board[9] == letter);
+        boolean option4 = (board[1] == letter && board[4] == letter && board[7] == letter);
+        boolean option5 = (board[2] == letter && board[5] == letter && board[8] == letter);
+        boolean option6 = (board[3] == letter && board[6] == letter && board[9] == letter);
+        boolean option7 = (board[1] == letter && board[5] == letter && board[9] == letter);
+        boolean option8 = (board[3] == letter && board[5] == letter && board[7] == letter);
+        if (option1 || option2 || option3 || option4 || option5 || option6 || option7 || option8) {
+
+            System.out.println("Player wins!!!");
+
+            return true;
+        }
+        if (board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' ' && board[5] != ' '
+                && board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ') {
+            System.out.println("Tie!!");
+            return true;
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Welcome To Tic Tac Toe Game");
         createBoard(board);
         getLetter();
         showBoard();
-        makeMove();
         playerMove();
         toss();
     }
